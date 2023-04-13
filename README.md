e6844cd311ceb27b76bb332e8c5faddcc58313f7
Cydiater <cydiater@gmail.com>
2023-04-13 10:48:52 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftRDP
+
+A description of this package.
---
+++ Tests/SwiftRDPTests/SwiftRDPTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftRDP
+
+final class SwiftRDPTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftRDP().text, "Hello, World!")
+    }
+}
